94717155b9a20d29fb3dd5409f904d6ee36ba349
YuvBhatt-Dev <204989587+YuvBhatt-YB@users.noreply.github.com>
2025-08-13 00:56:36 +0530

    Login with Google Button UI Fixed

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -69,14 +69,17 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent className=" font-secondary text-primary">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
+          <div className="flex flex-col gap-4">
+                <Button variant="outline" className="w-full" onClick={()=>{
+                  window.location.href = "http://localhost:8000/auth/google"
+                }}>
                   <FcGoogle />
                   Login with Google
                 </Button>
-              </div>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid gap-6 mt-4">
+              
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
                   Or continue with
