7c017d130c4382d42d783ff50d7c9e399a3c34ea
YuvBhatt-Dev <204989587+YuvBhatt-YB@users.noreply.github.com>
2025-09-17 22:16:17 +0530

    Added some new comment features

--- frontend/src/pages/MarketPage.jsx
+++ frontend/src/pages/MarketPage.jsx
@@ -7,8 +7,10 @@ import Trade from '@/components/MarketPage/Trade'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import api from "../api/markets"
+import { useSelector } from 'react-redux'
 const MarketPage = () => {
   const {marketId} = useParams()
+  const {profileImg} = useSelector((state)=>state.user.userData)
   const [marketData,setMarketData] = useState({})
   useEffect(()=>{
     const fetchMarketData = async () => {
@@ -22,6 +24,8 @@ const MarketPage = () => {
   },[])
   return (
     <div className=" max-width mx-auto px-2 lg:px-0 ">
+      <p>{marketId}</p>
+      <p>{profileImg}</p>
       <div className="  lg:flex lg:justify-between lg:items-start gap-3 py-6 ">
         <div className="flex-1">
           <DisplayChart marketData={marketData} />
